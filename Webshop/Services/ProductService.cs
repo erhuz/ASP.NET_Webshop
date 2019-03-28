b5ff0fe46c8b1fe0e61c2ea4a766c15116a2204f
Benjamin Fransson <benjamin@cafcon.se>
2019-03-28 13:13:58 +0100

    Added Product Service

--- Webshop/Services/ProductService.cs
+++ Webshop/Services/ProductService.cs
@@ -1,7 +1,62 @@
+using System.Collections.Generic;
+using System.Transactions;
+using Webshop.Models;
+using Webshop.Repositories;
+
 namespace Webshop.Services
 {
     public class ProductService
     {
-        
+        private readonly ProductRepository _productRepository;
+
+        public ProductService(ProductRepository productRepository)
+        {
+            this._productRepository = productRepository;
+        }
+
+        public List<Product> Get()
+        {
+            return this._productRepository.Get();
+        }
+
+        public Product Get(int id)
+        {
+            return this._productRepository.Get(id);
+        }
+
+        public bool Add(Product product)
+        {
+            if (
+                    product?.category_id == null ||
+                    string.IsNullOrEmpty(product?.title) ||
+                    string.IsNullOrEmpty(product?.description) ||
+                    product?.price == null
+            ){
+                return false;
+            }
+            
+            this._productRepository.Add(product);
+            
+            return true;
+        }
+
+        public bool Delete(int id)
+        {
+            using (var transaction = new TransactionScope())
+            {
+                var productItem = this._productRepository.Get(id);
+
+                if (productItem == null)
+                {
+                    return false;
+                }
+                
+                this._productRepository.Delete(id);
+                
+                transaction.Complete();
+
+                return true;
+            }
+        }
     }
 }
