7c2f563f0bdcba4b1117e278f0df57b2a90ebabe
Benjamin Fransson <benjamin@cafcon.se>
2019-04-01 23:50:04 +0200

    Added cart-item to retrieve data from request body

---
+++ Webshop/Models/CartItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Webshop.Models;
+
+namespace Webshop.Models
+{
+    public class CartItem
+    {
+        public int? CartId { get; set; }
+        public int ProductId{ get; set; }
+    }
+}
