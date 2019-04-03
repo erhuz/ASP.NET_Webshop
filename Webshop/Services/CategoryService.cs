b4b47a398f9d24e91c41ff95250a7ec7b178829c
Benjamin Fransson <benjamin@cafcon.se>
2019-04-03 19:06:49 +0200

    Renamed Service due to wrong naming convention

--- Webshop/Services/CategoryService.cs
+++ Webshop/Services/CategoryService.cs
@@ -7,9 +7,9 @@ namespace Webshop.Services
 {
     public class CategoryService
     {
-        private readonly CategoryRepository _categoryRepository;
+        private readonly ICategoryRepository _categoryRepository;
 
-        public CategoryService(CategoryRepository categoryRepository)
+        public CategoryService(ICategoryRepository categoryRepository)
         {
             this._categoryRepository = categoryRepository;
         }
